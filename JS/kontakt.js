f5790418fca69088865a0b304c06f0c20faf857b
SuneQuist44 <cotz44@gmail.com>
2020-09-22 14:33:57 +0200

    kontakt

--- JS/kontakt.js
+++ JS/kontakt.js
@@ -1,36 +1,36 @@
-        let btn = document.getElementById('contactButton'); //henter det valgte ID
-    btn.addEventListener("click", function() {
-    
-        let hasErrors = false;
-        
-        let name = document.getElementById("contactname"); //henter det valgte ID
-        if(!name.value) {
-            name.style.borderColor = 'red';
-            name.focus();
-            hasErrors = true;
-            return false;
-            //gør feltets kant rød hvis det er tomt, når der trykkes på 'send'
-        }
+let btn = document.getElementById('contactButton'); //henter det valgte ID
+btn.addEventListener("click", function () {
 
-        let email = document.getElementById("contactmail"); //henter det valgte ID
-        if(!email.value) {
-            email.style.borderColor = 'red';
-            email.focus();
-            hasErrors = true;
-            return false;
-            //gør feltets kant rød hvis det er tomt, når der trykkes på 'send'
-        }     
-        let message = document.getElementById("contactDescribe"); //henter det valgte ID
-        if(!message.value) {
-            message.style.borderColor = 'red';
-            message.focus();
-            hasErrors = true;
-            return false;
-            //gør feltets kant rød hvis det er tomt, når der trykkes på 'send'
-        }                   
-        if(!hasErrors) {
-            document.getElementById("contactform").submit();
-            //Lader formularen blive indsent hvis alle felter er udfyldt
-        }
+    let hasErrors = false;
 
-    });
+    let name = document.getElementById("contactname"); //henter det valgte ID
+    if (!name.value) {
+        name.style.border = "2px solid red"
+        name.focus();
+        hasErrors = true;
+        return false;
+        //gør feltets kant rød hvis det er tomt, når der trykkes på 'send'
+    }
+
+    let email = document.getElementById("contactmail"); //henter det valgte ID
+    if (!email.value) {
+        email.style.border = "2px solid red"
+        email.focus();
+        hasErrors = true;
+        return false;
+        //gør feltets kant rød hvis det er tomt, når der trykkes på 'send'
+    }
+    let message = document.getElementById("contactDescribe"); //henter det valgte ID
+    if (!message.value) {
+        message.style.border = "2px solid red"
+        message.focus();
+        hasErrors = true;
+        return false;
+        //gør feltets kant rød hvis det er tomt, når der trykkes på 'send'
+    }
+    if (!hasErrors) {
+        document.getElementById("contactform").submit();
+        //Lader formularen blive indsent hvis alle felter er udfyldt
+    }
+
+});
